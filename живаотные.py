2f2f9b9ec83f321df2f634e754f638ca097506f4
Vadim310031 <119740642+Vadim310031@users.noreply.github.com>
2023-01-16 16:00:46 +0300

    1

---
+++ живаотные.py
@@ -0,0 +1,10 @@
+class Annimals:
+    def __init__(self, name, vid="нету информации про вид", zv="Э"):
+        if name == "Ворона"or"ворона":
+            zv = "Кар"
+            self.zv = zv
+        elif name == "Курица"or"курица":
+            zv = "ko-ко-ko"
+            self.zv = zv
+        
+print(Annimals(input("Введите имя ")))
