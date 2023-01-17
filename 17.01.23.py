2121abbf70c671b0aa47b15af57fd8ca21c3fb4d
Vadim310031 <119740642+Vadim310031@users.noreply.github.com>
2023-01-17 18:01:26 +0300

    Вторая задача

--- 17.01.23.py
+++ 17.01.23.py
@@ -1,4 +1,17 @@
-def myfunc(a, b):
-    uvu = (a + b)/2
-    print(uvu)
-myfunc(4, 5)
+def bi(a:int):
+    c = ""
+    while a>1:
+        
+        c = str(a%2)+c
+        a = a//2
+    c = str(a)+c
+    return c
+def vos(a:int):
+    k = ""
+    while a>7:
+        
+        k = str(a%8)+k
+        a = a//8
+    k = str(a)+k
+    return k
+print(vos(17499))
