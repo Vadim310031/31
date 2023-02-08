8b1b95516721fd3aee1a6b67106d7c6087862e00
Vadim310031 <119740642+Vadim310031@users.noreply.github.com>
2023-02-08 19:55:21 +0300

    task

--- 17.01.23.py
+++ 17.01.23.py
@@ -1,17 +1,6 @@
-def bi(a:int):
-    c = ""
-    while a>1:
-        
-        c = str(a%2)+c
-        a = a//2
-    c = str(a)+c
-    return c
-def vos(a:int):
-    k = ""
-    while a>7:
-        
-        k = str(a%8)+k
-        a = a//8
-    k = str(a)+k
-    return k
-print(vos(17499))
+def perevod(v, os):
+    if v > 1:
+        perevod(v//os,os)
+    print(v%os)
+perevod(12,2)
+       
---
+++ task7.py/main.py
@@ -0,0 +1,6 @@
+n = int(input("Введите четное число или кратное 3   "   ))
+if (n%2 ==0) and (n%3==0):
+    print("TRUE")
+else:
+    print("FALSE")
+       
